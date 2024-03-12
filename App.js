8d57f4a0539a2366092b2d4700abc2da5c53e935
TalhaAhmed36 <41064991+TalhaAhmed36@users.noreply.github.com>
2024-03-12 01:56:25 -0700

    news app

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {Screen} from 'react-native-screens';
+import {createStackNavigator, assets} from '@react-navigation/stack';
+import Homepage from './Home';
+import {Platform} from 'react-native';
+import {useFonts} from 'expo-font';
+import {AppLoading} from 'expo';
+import Header from './Header';
+
+ 
+const Stack = createStackNavigator();
+
 
 export default function App() {
+let [fontsLoaded] = useFonts({
+'openSans': require('./assets/OpenSans-Semibold.ttf'),
+});
+if (!fontsLoaded){
+  return <AppLoading/>;
+} else {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer style={{paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight:0}} >
+      <Stack.Navigator initialRouteName="Globomantics" headerMode="screen">
+         </Stack.Navigator>
+      <Stack.Screen 
+      name="Globomantics"
+      component={Homepage}>
+        options={{
+          header: () => <Header headerDisplay="Globomantics"/>
+        }}
+      </Stack.Screen>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+}
